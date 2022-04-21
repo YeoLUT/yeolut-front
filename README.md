67117c83e868255663fe4a699cc035b98e644602
raonsol <raonsol@kakao.com>
2022-04-21 17:56:56 +0900

    ADD: React project

---
+++ README.md
@@ -0,0 +1,6 @@
+# 여럿 (YeoLUT)
+LUT 공유 플랫폼 프로젝트
+## Tech
+* React.js
+* WebAssembly
+  * [ffmpeg.WASM](https://github.com/ffmpegwasm/ffmpeg.wasm)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LutDemo from "./lutDemo.js";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <LutDemo></LutDemo>
+    </div>
+  );
+}
+
+export default App;
