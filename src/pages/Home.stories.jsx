278b26bd20040903620b9d3e8a6060373a541f5e
raonsol <raonsol@kakao.com>
2022-06-03 13:57:11 +0900

    CHORE: add home page to storybook

---
+++ src/pages/Home.stories.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Home from "./Home";
+
+export default {
+  title: "Main page",
+  component: Home,
+};
+
+export const Main = () => <Home />;
+
+// Popular.story = {
+//   parameters: {
+//     zeplinLink: [
+//       {
+//         name: "NavBar-popular",
+//         link: "zpl://components?pid=62875620de8426106a4f4785&coids=628c7c8de1264abd266cfb6e",
+//       },
+//     ],
+//   },
+// };
+// Search.story = {
+//   name: "Search",
+//   parameters: {
+//     zeplinLink: [
+//       {
+//         link: "zpl://components?coids=628c7c8c966153b94353642a&pid=62875620de8426106a4f4785",
+//       },
+//     ],
+//   },
+// };
+// Upload.story = {
+//   name: "Upload",
+//   parameters: {
+//     zeplinLink: [
+//       {
+//         link: "zpl://components?pid=62875620de8426106a4f4785&coids=628c7c8c38dc2fbe84fc72b4",
+//       },
+//     ],
+//   },
+// };
