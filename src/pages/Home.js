0b21e482cd65f4fca54e772a41c5ae04d210a5e2
raonsol <raonsol@kakao.com>
2022-06-16 03:23:32 +0900

    ADD: demo display options, reduce repeated code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,13 +5,56 @@ import AuthorCard from "../components/AuthorCard";
 import "./Home.css";
 import { MdExpandMore } from "react-icons/md";
 
+const isDemo = true;
+
 const publicImgSrc = (src) => {
   return process.env.PUBLIC_URL + src;
 };
 
+/**
+ *
+ * @param {*} url
+ */
+async function getList(url) {
+  const response = await fetch(url)
+    .then((response) => {
+      response.json();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  return response;
+}
+
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const createDemoAuthor = (n) => {
+  return [...new Array(n)].map((_, idx) => (
+    <AuthorCard
+      key={idx}
+      name={"Author" + idx}
+      likes={rand(10, 10000)}
+      downloads={rand(10, 10000)}
+      picSrc={"https://picsum.photos/100/100?random=" + idx}
+    />
+  ));
+};
+const createDemoLutCard = (n) => {
+  return [...new Array(n)].map((_, idx) => (
+    <CardItem
+      key={idx}
+      name={"LUT" + idx}
+      src={"https://picsum.photos/500/400?random=" + idx}
+      logname="log name"
+      author="Sample author"
+      download={rand(10, 10000)}
+      like={rand(10, 10000)}
+    />
+  ));
+};
+
 // eslint-disable-next-line require-jsdoc
 function Home() {
-  let imgCnt = 0;
   return (
     <div className="container-home">
       <nav className="nav">
@@ -26,175 +69,22 @@ function Home() {
             </span>
           </div>
           <div className="trend-content hs scrolling-wrapper">
-            <AuthorCard
-              name="SHAAM WORX"
-              likes={34000}
-              downloads={12900}
-              picSrc={"https://picsum.photos/100/100?random=" + imgCnt++}
-            />
-            <AuthorCard
-              name="SHAAM WORX"
-              likes={34000}
-              downloads={12900}
-              picSrc={"https://picsum.photos/100/100?random=" + imgCnt++}
-            />
-            <AuthorCard
-              name="SHAAM WORX"
-              likes={34000}
-              downloads={12900}
-              picSrc={"https://picsum.photos/100/100?random=" + imgCnt++}
-            />
-            <AuthorCard
-              name="Cube Frames"
-              likes={34000}
-              downloads={12900}
-              picSrc={"https://picsum.photos/100/100?random=" + imgCnt++}
-            />
-            <AuthorCard
-              name="J. T. Semple"
-              likes={34000}
-              downloads={12900}
-              picSrc={"https://picsum.photos/100/100?random=" + imgCnt++}
-            />
-            <AuthorCard
-              name="Ohad Peretz"
-              likes={34000}
-              downloads={12900}
-              picSrc={"https://picsum.photos/100/100?random=" + imgCnt++}
-            />
-            <AuthorCard
-              name="MediaKing"
-              likes={34000}
-              downloads={12900}
-              picSrc={"https://picsum.photos/100/100?random=" + imgCnt++}
-            />
-            <AuthorCard
-              name="Hong Gildong"
-              likes={34000}
-              downloads={12900}
-              picSrc={"https://picsum.photos/100/100?random=" + imgCnt++}
-            />
-            <AuthorCard
-              name="Zodiac"
-              likes={34000}
-              downloads={12900}
-              picSrc={"https://picsum.photos/100/100?random=" + imgCnt++}
-            />
+            {isDemo && createDemoAuthor(10)}
           </div>
         </div>
         <div className="container-filters">
           <button className="btn-primary btn-font"> 썸네일 업로드 </button>
           <button className="btn-dropdown btn-font">
-            {" "}
-            Gamma <MdExpandMore />{" "}
+            Gamma <MdExpandMore />
           </button>
           <button className="btn-dropdown btn-font">
-            {" "}
-            Style <MdExpandMore />{" "}
+            Style <MdExpandMore />
           </button>
           <button className="btn-dropdown btn-font">
-            {" "}
-            Color <MdExpandMore />{" "}
+            Color <MdExpandMore />
           </button>
         </div>
-        <div className="body-home">
-          <CardItem
-            name="Killstreak"
-            src={"https://picsum.photos/500/400?random=" + imgCnt++}
-            logname="Other"
-            author="Cube Frames"
-            download={805}
-            like={805}
-          />
-          <CardItem
-            name="THRILLER"
-            src={"https://picsum.photos/500/400?random=" + imgCnt++}
-            logname="DJI D-log"
-            author="SHAAM WORX"
-            download={805}
-            like={805}
-          />
-          <CardItem
-            name="LateAfternoonWanderlust"
-            src={"https://picsum.photos/500/400?random=" + imgCnt++}
-            logname="Canon Cinestyle"
-            author="J. T. Semple"
-            download={805}
-            like={805}
-          />
-          <CardItem
-            name="CINEMA 2"
-            src={"https://picsum.photos/500/400?random=" + imgCnt++}
-            logname="Rec 709"
-            author="Andy"
-            download={805}
-            like={805}
-          />
-          <CardItem
-            name="TEAL LOOK FOR REC 709 V2"
-            src={"https://picsum.photos/500/400?random=" + imgCnt++}
-            logname="Rec 709"
-            author="tjtop"
-            download={805}
-            like={805}
-          />
-          <CardItem
-            name="Hypnosis"
-            src={"https://picsum.photos/500/400?random=" + imgCnt++}
-            logname="Panasonic V-log"
-            author="Cube Frames"
-            download={805}
-            like={805}
-          />
-          <CardItem
-            name="cinematic green"
-            src={"https://picsum.photos/500/400?random=" + imgCnt++}
-            logname="Rec 709"
-            author="pushpak dsilva"
-            download={805}
-            like={805}
-          />
-          <CardItem
-            name="BMP4K PEAKY BLİNDERS"
-            src={"https://picsum.photos/500/400?random=" + imgCnt++}
-            logname="Blackmagic BMD Film 4K"
-            author="berat"
-            download={805}
-            like={805}
-          />
-          <CardItem
-            name="Cine Teal"
-            src={"https://picsum.photos/500/400?random=" + imgCnt++}
-            logname="Other"
-            author="I WANT TO MAKE A FILM PODCAST"
-            download={805}
-            like={805}
-          />
-          <CardItem
-            name="Relatives | Creative LUT"
-            src={"https://picsum.photos/500/400?random=" + imgCnt++}
-            logname="Rec.709"
-            author="Alexander Barrels"
-            download={805}
-            like={805}
-          />
-          <CardItem
-            name="True Colors"
-            src={"https://picsum.photos/500/400?random=" + imgCnt++}
-            logname="Sony S-Log 3"
-            author="Ohad Peretz"
-            download={805}
-            like={805}
-          />
-          <CardItem
-            name="Vintage Warmth"
-            src={"https://picsum.photos/500/400?random=" + imgCnt++}
-            logname="Sony S-Log 3"
-            author="Ohad Peretz"
-            download={805}
-            like={805}
-          />
-        </div>
+        <div className="body-home">{isDemo && createDemoLutCard(30)}</div>
       </main>
     </div>
   );
